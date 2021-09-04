dabe84a96704933bea4f25b29b13af3e5cb636f5
alfredo <alfredomartinezruiz01@gmail.com>
2021-09-03 21:47:22 -0500

    Listo para compresión/descompresión

--- src/PruebaS.java
+++ src/PruebaS.java
@@ -8,20 +8,16 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class PruebaS {
+    
     static int puerto = 8000;
     static String direccion = "192.168.0.109";
+    
     public static void main(String[] args){
       try{
             // Creamos un folder provicional de prueba
             File f = new File("");
             String ruta = f.getAbsolutePath();
           
-          
-            /**
-                  * Argumentos para envío y recepción de datos con el servidor
-                  *  0: recibiendo archivo
-                  *  1: recibiendo directorio
-            */
             ServerSocket skt_server = conectar(puerto);
             skt_server.setReuseAddress(true);
             System.out.println("Servidor escuchando en: " + direccion + ":" + puerto);
@@ -33,47 +29,11 @@ public class PruebaS {
                 System.out.println("Cliente conectado desde " + skt_cliente.getInetAddress() + ":" + skt_cliente.getPort());
                 
                 DataInputStream dis = new DataInputStream(skt_cliente.getInputStream());
-                //int n_elementos = dis.read(); // Leemos el número de elementos enviados por cliente
-                int n_elementos = 3;
-                
-                for(int e_recibidos = 0; e_recibidos < n_elementos; e_recibidos++){
-                    //int comando = dis.read(); // Leemos el comando envíado por el cliente
-                    int comando = 0;
-                    if(comando == 0){
-                            try {
-                                // Leemos metadatos del archivo
-                                String nombre = dis.readUTF();
-                                long tam = dis.readLong();
-
-                                try ( // Creamos el flujo de salida al archivo
-                                        DataOutputStream dos = new DataOutputStream(new FileOutputStream(ruta + "\\" + nombre))) {
-                                    System.out.println(ruta + "\\" + nombre);
-                                    System.out.println(tam);
-                                    // Recibiendo archivo
-                                    int recibidos;
-                                    for(long escritos = 0; escritos < tam; ){
-                                        byte[] b = new byte[1500]; // Recibimos un paquete de 1500 bytes
-                                        recibidos = dis.read(b);
-                                        
-                                        // dos.write(b, 0, recibidos); // Leemos desde el byte 0 hasta el número de leídos del socket
-                                        // dos.flush();
-                                        
-                                        escritos += recibidos;
-                                        System.out.println("Recibidos: " + escritos + " tam " + tam);
-                                    }
-                                    System.out.println("Archivo: " + ruta + nombre + " recibido correctamente.");
-                                }
-
-                            } catch (Exception e) {
-                                System.out.println("No se pudo recibir el archivo del cliente.");
-                                e.printStackTrace();
-                            }
-                    }         
-                // dis.close();
-                // skt_cliente.close();
-            }
-            
-            
+                File comprimido = reciveFile(dis, ruta);
+                descomprimir(comprimido);
+                           
+                dis.close();
+                skt_cliente.close();  
             }  
       }catch(Exception e){
           e.printStackTrace();
@@ -83,5 +43,44 @@ public class PruebaS {
      // Abrimos un puerto del servidor
     public static ServerSocket conectar(int puerto) throws IOException{
         return new ServerSocket(puerto);
+    }
+    
+    // Descomprimimos el archivo
+    public static boolean descomprimir(File arch){
+        
+        return false;
+    }
+    
+    // Recibimos cada archivo enviado por el cliente
+    public static File reciveFile(DataInputStream dis, String ruta){
+        try {
+            // Leemos metadatos del archivo
+            String nombre = dis.readUTF();
+            long tam = dis.readLong();
+            File arch = new File(ruta + "\\" + nombre);
+            
+            // Creamos el flujo de salida al archivo
+            DataOutputStream dos = new DataOutputStream(new FileOutputStream(arch.getAbsolutePath()));
+            
+            // Recibiendo archivo
+            int recibidos;
+            for(long escritos = 0; escritos < tam; ){
+                byte[] b = new byte[1500]; // Recibimos un paquete de 1500 bytes
+                recibidos = dis.read(b);
+                
+                dos.write(b, 0, recibidos); // Leemos desde el byte 0 hasta el número de leídos del socket
+                dos.flush();
+                escritos += recibidos;
+                System.out.println("Recibidos: " + escritos + " tam " + tam);
+            }            
+            System.out.println("Archivo: " + ruta + nombre + " recibido correctamente.");
+            
+            return arch;
+            
+        } catch (Exception e) {
+            System.out.println("No se pudo recibir el archivo del cliente.");
+            e.printStackTrace();
+            return null;
+        }
     }
 }   
--- src/PruebasC.java
+++ src/PruebasC.java
@@ -1,4 +1,3 @@
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
@@ -9,62 +8,24 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFileChooser;
 
-
 public class PruebasC {
     public static void main(String[] args){
+        
         int puerto = 8000;
-        String direccion = "192.168.0.109";
+        String direccion = "192.168.0.109"; // Dirección IP 
         
         try {
             Socket skt_cliente = conectar(puerto, direccion); // La direccion dependerá del la IP del servidor
             System.out.println("Conexión establecida en: " + direccion + ":" + puerto);
             File[] seleccion = getChoice();
-            
-            
-             // Creamos el flujo de escritura
-            DataOutputStream dos = new DataOutputStream(skt_cliente.getOutputStream());
-            //dos.write(seleccion.length); // Enviamos el número de elementos
-            
-            for(File arch : seleccion){
-                if(arch.isDirectory()){
-                    dos.write(1); // Indicamos el envío de un directorio
-                    
-                } else{
-                    //dos.write(0); // Indicamos el envío de un archivo
-                    
-                    String nombre = arch.getName();
-                    long tam = arch.length();
-                    String ruta = arch.getAbsolutePath();
-
-                    try {  
-                        // Creamos el flujo de escritura
-                        DataInputStream dis = new DataInputStream(new FileInputStream(ruta));
-
-                        // Enviando metadatos del archivo
-                        dos.writeUTF(nombre); // Indicamos el nombre
-                        dos.writeLong(tam); // Indicando el tamaño en bytes
-                        //dos.flush();
                         
-                        // Enviando archivo
-                        int leidos;
-                        for(long enviados = 0; enviados < tam; ){
-                            byte[] b = new byte[1500]; // Enviamos en paquete de 1500 bytes
-                            leidos = dis.read(b);
-                            
-                            dos.write(b, 0, leidos); // Leemos desde el byte 0 hasta el número de leídos del archivo
-                            dos.flush();
-                            enviados += leidos;
-                            System.out.println("Enviados: " + enviados + " tam " + tam);
-                        }
-
-                        System.out.println("Archivo: " + ruta + " enviado correctamente.");              
-
-                    } catch (Exception e) {
-                        System.out.println("No se pudo enviar el archivo: " + ruta);
-                        e.printStackTrace();
-                    }
-                }
-            }
+            // Compresión de archivos            
+            File comprimido = comprimir(seleccion);
+            
+            // Creamos el flujo de escritura
+            DataOutputStream dos = new DataOutputStream(skt_cliente.getOutputStream());
+            sendFile(comprimido, dos);
+            
             dos.close();
             skt_cliente.close();
             
@@ -79,6 +40,7 @@ public class PruebasC {
     public static Socket conectar(int puerto, String direccion) throws IOException{
         return new Socket(direccion, puerto);
     }
+    
     // Obtenemos lo que seleccionó para subir
     public static File[] getChoice(){
         // Lanzamos el file chooser para elegir carpetas o directorios
@@ -95,4 +57,45 @@ public class PruebasC {
         return archs;
     }
     
+    // Comprimir archivos
+    public static File comprimir(File archs[]){
+        
+        return null;
+    }
+    
+    // Enviamos un archivo
+    public static void sendFile(File arch, DataOutputStream dos){
+        String nombre = arch.getName();
+        long tam = arch.length();
+        String ruta = arch.getAbsolutePath();
+        
+        try {  
+            // Creamos el flujo de escritura
+            DataInputStream dis = new DataInputStream(new FileInputStream(ruta));
+            
+            // Enviando metadatos del archivo
+            dos.writeUTF(nombre); // Indicamos el nombre
+            dos.writeLong(tam); // Indicando el tamaño en bytes
+            dos.flush();
+            
+            // Enviando archivo
+            int leidos;
+            for(long enviados = 0; enviados < tam; ){
+                byte[] b = new byte[1500]; // Enviamos en paquete de 1500 bytes
+                leidos = dis.read(b);
+                
+                dos.write(b, 0, leidos); // Leemos desde el byte 0 hasta el número de leídos del archivo
+                dos.flush();
+                enviados += leidos;
+                System.out.println("Enviados: " + enviados + " tam " + tam);
+            }
+            
+            System.out.println("Archivo: " + ruta + " enviado correctamente.");              
+            
+        } catch (Exception e) {
+            System.out.println("No se pudo enviar el archivo: " + ruta);
+            e.printStackTrace();
+        }
+        
+    }
 }
